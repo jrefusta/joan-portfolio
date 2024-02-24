cef8ae74d2b00833022db687da6132bfe8084835
Joan Ramos Refusta <jrefusta@gmail.com>
2024-02-24 12:46:01 +0100

    remove unused line

--- src/Experience/Whiteboard.js
+++ src/Experience/Whiteboard.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import Experience from "./Experience.js";
-import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer.js";
 
 export default class Whiteboard {
   constructor() {
