034a8f480d17b4c9a2f9b821d81fb214a8d27899
Joan Ramos Refusta <jrefusta@gmail.com>
2024-03-01 15:20:46 +0100

    update coffeSteam code

--- src/Experience/CoffeeSteam.js
+++ src/Experience/CoffeeSteam.js
@@ -17,7 +17,9 @@ export default class CoffeeSteam {
     this.model = {};
 
     this.model.color = "#d1d1d1";
-
+    this.model.perlinTexture = this.resources.items.perlin;
+    this.model.perlinTexture.wrapS = THREE.RepeatWrapping;
+    this.model.perlinTexture.wrapT = THREE.RepeatWrapping;
     // Material
     this.model.material = new THREE.ShaderMaterial({
       transparent: true,
@@ -27,24 +29,23 @@ export default class CoffeeSteam {
       fragmentShader,
       uniforms: {
         uTime: { value: 0 },
-        uTimeFrequency: { value: 0.001 },
-        uUvFrequency: { value: new THREE.Vector2(3, 5) },
-        uColor: { value: new THREE.Color(this.model.color) },
+        uPerlinTexture: { value: this.resources.items.perlin },
       },
     });
 
     // Mesh
-    this.planeGeom = new THREE.PlaneGeometry(0.2, 0.7);
+    this.planeGeom = new THREE.PlaneGeometry(0.15, 0.6, 16, 64);
     this.model.mesh = new THREE.Mesh(this.planeGeom, this.model.material);
-    this.model.mesh.position.set(0.234979, 2.1, -3.64951);
+    this.model.mesh.position.set(0.230979, 2.3, -3.64951);
     this.model.mesh2 = new THREE.Mesh(this.planeGeom, this.model.material);
     this.model.mesh2.rotateY(-Math.PI / 2);
     this.model.mesh2.position.set(0.234979, 2.1, -3.64951);
-    this.scene.add(this.model.mesh2);
     this.scene.add(this.model.mesh);
+    //this.scene.add(this.model.mesh2);
   }
 
   update() {
-    this.model.material.uniforms.uTime.value = this.time.elapsed;
+    const elapsedTime = this.experience.clock.getElapsedTime();
+    this.model.material.uniforms.uTime.value = elapsedTime;
   }
 }
