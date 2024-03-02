c93d101c724552c6ffc8db0d6ef3fadc910d3b74
Joan Ramos Refusta <jrefusta@gmail.com>
2024-03-02 15:48:00 +0100

    updated arcade url

--- src/Experience/ArcadeScreen.js
+++ src/Experience/ArcadeScreen.js
@@ -42,7 +42,7 @@ export default class ArcadeScreen {
 
     const iframe = document.createElement("iframe");
 
-    iframe.src = "http://192.168.1.72:8081/";
+    iframe.src = "https://joan-arcade-machine.vercel.app";
     iframe.style.width = this.screenSize.width + "px";
     iframe.style.height = this.screenSize.height + "px";
     iframe.style.padding = 16 + "px";
